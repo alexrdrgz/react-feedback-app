3e0f803855124585251966eb102dee2e9eddf645
Alex Rodriguez <contact@alexrodriguez.io>
2022-01-12 11:14:45 -0800

    (feedbacklist.jsx): added animations using library

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,5 @@
+import {motion, AnimatePresence} from 'framer-motion'
+import PropTypes from 'prop-types'
 import FeedbackItem from "./FeedbackItem"
 
 function FeedbackList({feedback, handleDelete}){
@@ -6,12 +8,25 @@ function FeedbackList({feedback, handleDelete}){
         return 'No Feedback Yet'
     }
 
+    // return (
+
+    //     <div className="feedback-list">
+    //         {feedback.map( (item) => (
+    //             <FeedbackItem handleDelete={handleDelete} key={item.id} item={item} />
+    //         ))}
+    //     </div>
+    // )
+
     return (
 
         <div className="feedback-list">
-            {feedback.map( (item) => (
-                <FeedbackItem handleDelete={handleDelete} key={item.id} item={item} />
-            ))}
+            <AnimatePresence>
+                {feedback.map( (item) => (
+                    <motion.div key={item.id} initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
+                        <FeedbackItem handleDelete={handleDelete} key={item.id} item={item} />
+                    </motion.div>
+                ))}
+            </AnimatePresence>
         </div>
     )
     
